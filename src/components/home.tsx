1b4234b49ce5e5f76e3dfa54dc37379ea562d4e7
Joel Odak <joel.odak@lintonsbeauty.com>
2025-04-17 20:06:37 +0300

    2

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,36 +1,56 @@
-import { FiChevronDown } from 'react-icons/fi';
-import LogoWhite from 'assets/images/Hair Academy Logo White.png?w=480;640;1280;1980&format=webp';
-import Image1 from 'assets/images/backdrop.png?w=480;640;1280;1980&format=webp';
+import { useState, useEffect } from 'react'
+import LogoWhite from 'assets/images/Hair Academy Logo White.png?w=480;640;1280;1980&format=webp'
+import Image1 from 'assets/images/backdrop.png?w=480;640;1280;1980&format=webp'
+import { join, map } from 'lodash-es'
+import { FiChevronDown } from 'react-icons/fi'
 
-const widths = [480, 640, 1280, 1980, 853];
+const images = [Image1]
+const widths = [480, 640, 1280, 1980, 853]
 
 export function Home() {
+  const [currentImage, setCurrentImage] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev + 1) % images.length)
+    }, 5000)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
-    <main
+    <div
       id="home"
       className="relative flex h-screen min-h-screen w-full flex-col items-center justify-center text-primary-foreground"
     >
       <picture className="absolute inset-0 h-full w-full">
         <source
-          srcSet={`${Image1} 480w, ${Image1} 640w, ${Image1} 1280w, ${Image1} 1980w`}
-          sizes="100vw"
+          srcSet={join(
+            map(
+              images[currentImage] as string[],
+              (image, index) => `${image} ${widths[index]}w`
+            ),
+            ', '
+          )}
+          sizes="100vw" // Ensure the image scales properly for mobile
         />
         <img
-          src={Image1}
-          alt="Background showcasing the academy"
+          src={images[currentImage][0]}
+          alt="Background image"
           className="h-full w-full object-cover object-center transition-opacity duration-1000"
         />
       </picture>
       <div className="absolute flex h-full w-full items-center justify-center bg-black/60 flex-col">
         <picture className="h-auto w-4/5 md:w-3/5 lg:w-1/3">
           <source
-            srcSet={`${LogoWhite} 480w, ${LogoWhite} 640w, ${LogoWhite} 1280w, ${LogoWhite} 1980w`}
+            srcSet={join(
+              map(
+                LogoWhite as string[],
+                (image, index) => `${image} ${widths[index]}w`
+              ),
+              ', '
+            )}
           />
-          <img
-            src={LogoWhite}
-            alt="Lintons Hair Academy Logo"
-            className="h-auto w-full object-contain"
-          />
+          <img src={LogoWhite[0]} className="h-auto w-full object-contain" />
         </picture>
         <a
           href="https://forms.gle/MJreqbGZV4uVXfvMA"
@@ -42,6 +62,6 @@ export function Home() {
         </a>
         <FiChevronDown className="absolute bottom-8 h-12 w-12 md:h-16 md:w-16" />
       </div>
-    </main>
-  );
+    </div>
+  )
 }
